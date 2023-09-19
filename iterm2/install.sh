50471af0700838fdb1bb98902a20ce9732682bcc
Kang Hyun Bu <crazyatom84@gmail.com>
2023-09-19 15:02:02 +0900

    Update iterm2

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,5 +1,10 @@
 # !/bin/bash
 
-# cp ./iterm2/com.googlecode.iterm2.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles/
+# Specify the preferences directory
+mkdir -p ~/.config/iterm2/custom/
+cp ./iterm2/com.googlecode.iterm2.plist ~/.config/iterm2/custom/
 
-chsh -s /bin/zsh
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2/custom/"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
