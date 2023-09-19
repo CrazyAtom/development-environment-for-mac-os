4e5d6e1cda06cb02cd929acf07a3ad5580574289
Kang Hyun Bu <crazyatom84@gmail.com>
2023-09-19 14:48:33 +0900

    Create dock.sh

---
+++ macos/dock.sh
@@ -0,0 +1,20 @@
+defaults write com.apple.dock show-recents -bool false
+
+defaults write com.apple.dock persistent-apps -array
+sleep 10
+killall Dock
+apps=(
+  "/System/Applications/Launchpad.app"
+  "/Applications/Google Chrome.app"
+  "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
+  "/Applications/iTerm.app"
+  "/Applications/Visual Studio Code.app"
+  "/Applications/Figma.app"
+  "/Applications/Postman.app"
+  "/System/Library/CoreServices/Applications/Screen Sharing.app"
+)
+for app in "${apps[@]}"; do
+    defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${app}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+done
+sleep 10
+killall Dock
